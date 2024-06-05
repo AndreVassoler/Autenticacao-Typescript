19f18b3900cded0deab7d865fb455c8bd44137e2
André <111258213+AndreVassoler@users.noreply.github.com>
2024-06-05 23:38:49 +0000

    Aula 05-06

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 import { NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { ProductMiddleware } from './products/product.middleware';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://0.0.0.0/books-nest'),
     ProductsModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
